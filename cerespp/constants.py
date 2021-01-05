43f2b29b10e25c495ab1317163e3fb1e5ffed6fa
José Vines <jose.vines@ug.uchile.cl>
2021-01-05 20:08:10 -0300

    Added some constants.

---
+++ cerespp/constants.py
@@ -0,0 +1,19 @@
+"""constants.py
+This module contains constants to be used throughout the code.
+I'm storing them here to remove clutter from the main code.
+"""
+
+# relevant lines
+CaH = 3968.47
+CaK = 3933.664
+CaV = 3901
+CaR = 4001
+Ha = 6562.808
+HeI = 5875.62
+NaID1 = 5895.92
+NaID2 = 5889.95
+
+# Plotting
+fontsize = 22
+fontname = 'serif'
+tick_labelsize = 18
