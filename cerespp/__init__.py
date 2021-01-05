2fd8fee486e49874071ca25299aa50245a5623f2
José Vines <jose.vines@ug.uchile.cl>
2021-01-05 20:07:51 -0300

    Initial commit

---
+++ cerespp/__init__.py
@@ -0,0 +1,6 @@
+"""
+Ceres plusplus is a module designed to extend the functionality of ceres
+(https://github.com/rabrahm/ceres) in order to calculate activity indicators
+from spectra reduced with the tool, and to easily extract the indicators that
+ceres does calculate, for example the CCF FWHM and the BIS.
+"""
