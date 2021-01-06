6e47a470795b9c29da36a75d2fa050ba37f02fc8
José Vines <jose.vines@ug.uchile.cl>
2021-01-05 22:31:37 -0300

    Updated __init__.py

--- cerespp/__init__.py
+++ cerespp/__init__.py
@@ -4,3 +4,14 @@ Ceres plusplus is a module designed to extend the functionality of ceres
 from spectra reduced with the tool, and to easily extract the indicators that
 ceres does calculate, for example the CCF FWHM and the BIS.
 """
+
+from .cerespp import get_activities
+from .cpplots import ccf_gauss_plot
+from .cpplots import ccf_plot
+from .cpplots import line_plot
+from .crosscorr import ccf
+from .crosscorr import ccf_fit
+from .spectra_utils import median_combine
+from .spectra_utils import median_combine_1d
+from .spectra_utils import merge_echelle
+from .spectra_utils import velocity_correction
