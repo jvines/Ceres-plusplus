9a69662d773a9e7ee952826665920a0a83d1267b
José Vines <jose.vines@ug.uchile.cl>
2021-02-22 13:22:36 -0300

    Added create_dir function

--- cerespp/utils.py
+++ cerespp/utils.py
@@ -1,3 +1,5 @@
+import os
+
 import numpy as np
 from scipy.integrate import trapz
 from scipy.interpolate import interp1d
@@ -58,3 +60,18 @@ def get_line_flux(wavelength, flux, line, width, filt='square', error=None):
         sigma = np.sqrt(var)
         return integrated_flux, sigma
     return integrated_flux
+
+
+def create_dir(path):
+    """Create a directory."""
+    try:
+        os.mkdir(path)
+    except OSError:
+        err_msg = f"Creation of the directory {path:s} failed. "
+        err_msg += "It might already exist"
+        print(err_msg)
+        pass
+    else:
+        print(f"Created the directory {path:s} ")
+        pass
+    pass
